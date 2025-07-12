ca3a3862e64ce7f62ae1729dfc8386d62b0f16c4
mochalins <117967760+mochalins@users.noreply.github.com>
2025-07-13 02:05:04 +0900

    fix: Remove deprecated std API usage

--- build.zig
+++ build.zig
@@ -21,10 +21,12 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     const mod_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/serialport.zig"),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/serialport.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
     });
     if (target.result.os.tag == .windows) {
         mod_unit_tests.root_module.linkSystemLibrary("advapi32", .{});
@@ -35,16 +37,14 @@ pub fn build(b: *std.Build) void {
     if (lib) |l| {
         // Library Artifact
         {
-            const library = if (l == .static) b.addStaticLibrary(.{
+            const library = b.addLibrary(.{
                 .name = "serialport",
-                .root_source_file = b.path("src/lib.zig"),
-                .target = target,
-                .optimize = optimize,
-            }) else b.addSharedLibrary(.{
-                .name = "serialport",
-                .root_source_file = b.path("src/lib.zig"),
-                .target = target,
-                .optimize = optimize,
+                .root_module = b.createModule(.{
+                    .root_source_file = b.path("src/lib.zig"),
+                    .target = target,
+                    .optimize = optimize,
+                }),
+                .linkage = l,
             });
             library.root_module.addImport("serialport", mod);
             b.installArtifact(library);
@@ -52,10 +52,12 @@ pub fn build(b: *std.Build) void {
         // Library Tests
         {
             const lib_unit_tests = b.addTest(.{
-                .root_source_file = b.path("src/lib.zig"),
-                .target = target,
-                .optimize = optimize,
-                .link_libc = true,
+                .root_module = b.createModule(.{
+                    .root_source_file = b.path("src/lib.zig"),
+                    .target = target,
+                    .optimize = optimize,
+                    .link_libc = true,
+                }),
             });
             lib_unit_tests.root_module.addImport("serialport", mod);
             const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
